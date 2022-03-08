215c8d7e08e38b589e9a9b9c15dfe91303f78d03
a33372 <hai.nguyen592000@gmail.com>
2022-03-08 13:29:31 +0700

    #1 completed header, banner, course, add source code login and register form

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,18 +1,29 @@
+let loginForm = document.getSelection('.login-form');
+let btnLoginRegister = document.querySelector('.login-register-btn');
 let navbar = document.querySelector('.nav');
-let menu = document.querySelector('#menu-bar')
+let menu = document.querySelector('#menu-bar');
+
+// btnLoginRegister.onclick = () => {
+// 	loginForm.classList.add('active');
+// }
+
+// document.querySelector('.close-login-form').onclick = () => {
+// 	loginForm.classList.remove('active');
+// }
 
 menu.onclick = () => {
-	menu.classList.toggle('fa-times')
+	menu.classList.toggle('fa-times');
 	navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-	menu.classList.remove('fa-times')
-	navbar.classList.remove('active');
+  // loginForm.classList.remove('active');
+	menu.classList.remove('fa-times');
+	navbar.classList.remove('active');  
 
-	if (window.scrollY > 0) {
-		document.querySelector('#header').classList.add('active');
-	}else{
-		document.querySelector('#header').classList.remove('active');
-	}
+	// if (window.scrollY > 0) {
+	// 	document.querySelector('.header').classList.add('active');
+	// }else{
+	// 	document.querySelector('.header').classList.remove('active');
+	// }
 }
