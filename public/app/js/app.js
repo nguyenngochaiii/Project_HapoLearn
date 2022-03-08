31c16d6bbeebd3de98fd62918a42df56903d2cdb
a33372 <hai.nguyen592000@gmail.com>
2022-03-08 14:31:16 +0700

    #1 fixed comment code

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -3,27 +3,12 @@ let btnLoginRegister = document.querySelector('.login-register-btn');
 let navbar = document.querySelector('.nav');
 let menu = document.querySelector('#menu-bar');
 
-// btnLoginRegister.onclick = () => {
-// 	loginForm.classList.add('active');
-// }
-
-// document.querySelector('.close-login-form').onclick = () => {
-// 	loginForm.classList.remove('active');
-// }
-
 menu.onclick = () => {
 	menu.classList.toggle('fa-times');
 	navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
-  // loginForm.classList.remove('active');
 	menu.classList.remove('fa-times');
 	navbar.classList.remove('active');  
-
-	// if (window.scrollY > 0) {
-	// 	document.querySelector('.header').classList.add('active');
-	// }else{
-	// 	document.querySelector('.header').classList.remove('active');
-	// }
 }
