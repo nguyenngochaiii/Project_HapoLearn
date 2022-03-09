883f79c118d78fde9a13cff4d7d07d60fe699473
a33372 <hai.nguyen592000@gmail.com>
2022-03-09 11:30:48 +0700

    update fixed comment

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,7 +1,5 @@
-let loginForm = document.getSelection('.login-form');
-let btnLoginRegister = document.querySelector('.login-register-btn');
 let navbar = document.querySelector('.nav');
-let menu = document.querySelector('#menu-bar');
+let menu = document.querySelector('.menu-bar');
 
 menu.onclick = () => {
 	menu.classList.toggle('fa-times');
