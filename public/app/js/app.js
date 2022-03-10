3ff7706038cc3c90f431ec8e6879b6a0864f745c
a33372 <hai.nguyen592000@gmail.com>
2022-03-10 11:20:53 +0700

    update

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
 	$(".menu-bar").click(function () {
 	  $(".nav").toggleClass('active');
 	});
+
+  $("#login_register").click(function () {
+	  $("#login").toggleClass('active');
+	});
+
+  $("#login-btn").click(function () {
+	  $("#login").toggleClass('active');
+	});
+
+  $("#register-btn").click(function () {
+	  $("#register").toggleClass('active');
+	});
+
 });
