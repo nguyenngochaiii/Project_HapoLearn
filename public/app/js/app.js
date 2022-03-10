1eb4df1c1959743a46febecc075d8ea5be2f587f
a33372 <hai.nguyen592000@gmail.com>
2022-03-10 10:46:41 +0700

    update

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,12 +1,5 @@
-let navbar = document.querySelector('.nav');
-let menu = document.querySelector('#menu-bar');
-
-menu.onclick = () => {
-	menu.classList.toggle('fa-times');
-	navbar.classList.toggle('active');
-}
-
-window.onscroll = () => {
-	menu.classList.remove('fa-times');
-	navbar.classList.remove('active');  
-}
+$(document).ready(function () {
+	$(".menu-bar").click(function () {
+	  $(".nav").toggleClass('active');
+	});
+});
