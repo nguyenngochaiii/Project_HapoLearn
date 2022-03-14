ce1627bfbe7ba522c38ba80eba6bb6c28c04998c
a33372 <hai.nguyen592000@gmail.com>
2022-03-14 10:00:51 +0700

    update code

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-	$(".menu-bar").click(function () {
-		$(".nav").toggleClass('active');
-	});
+  $(".menu-bar").click(function () {
+    $(".nav").toggleClass('active');
+  });
+
+  $("#login-register").click(function () {
+    $("#login").toggleClass('active');
+  });
+
+  $("#login-btn").click(function () {
+    $("#login").toggleClass('active');
+  });
+
+  $("#register-btn").click(function () {
+    $("#register").toggleClass('active');
+  });
+
+
 });
